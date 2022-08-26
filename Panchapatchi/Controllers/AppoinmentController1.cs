fc3fe25ebde6b49f0f0b0d88b8dd10d95d67d8a5
rama adhi <ramnltd@msn.com>
2022-08-26 17:37:05 +0100

    ini commit

---
+++ Panchapatchi/Controllers/AppoinmentController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Panchapatchi.Controllers
+{
+    public class AppoinmentController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            string todaysDeate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDeate);
+        }
+    }
+}
